38b415a1d9fb700e155c38b52129ece543d3a086
ChristVolo1 <christy.volosin@gmail.com>
2016-12-11 21:07:24 -0600

    I added background color and border, and moved the zombie down the page.

--- braineaters.js
+++ braineaters.js
@@ -1,43 +1,19 @@
-var canvas;
-var ctx;
-var cCircle = (function () {
-    function cCircle(x, y, radius, color, line_width) {
-        if (color === void 0) { color = "red"; }
-        if (line_width === void 0) { line_width = 2; }
-        var _this = this;
-        this.x = 0;
-        this.y = 0;
-        this.radius = 10;
-        this.lineWidth = 2;
-        this.color = "red";
-        this.draw = function () {
-            ctx.save();
-            ctx.beginPath();
-            ctx.strokeStyle = _this.color;
-            ctx.lineWidth = _this.lineWidth;
-            ctx.arc(_this.x, _this.y, _this.radius, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.restore();
-        };
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
-        this.lineWidth = line_width;
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(20, 2, 970, 600);
+ctx.fillStyle = 'indigo';
+ctx.fill();
+ctx.lineWidth = 7;
+ctx.strokeStyle = 'black';
+ctx.stroke();
+var zombie = new Image();
+zombie.onload = function loop() {
+    for (var i = 400; i < 500; i++) {
+        ctx.drawImage(zombie, i, i);
+        function moveZombie() {
+            setInterval(this.zombie, 500);
+        }
     }
-    return cCircle;
-}());
-var circle1 = new cCircle(200, 300, 50);
-var circle2 = new cCircle(400, 550, 150, "blue", 5);
-function gameLoop() {
-    requestAnimationFrame(gameLoop);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, 1280, 720);
-    circle1.draw();
-    circle2.draw();
-}
-window.onload = function () {
-    canvas = document.getElementById('cnvs');
-    ctx = canvas.getContext("2d");
-    gameLoop();
 };
+zombie.src = "tiny-geek-zombie-icon.jpg";
--- braineaters.ts
+++ braineaters.ts
@@ -1,50 +1,95 @@
-let canvas: HTMLCanvasElement;
-let ctx: CanvasRenderingContext2D;
+let canvas=<HTMLCanvasElement>document.getElementById('myCanvas');
+let ctx = canvas.getContext("2d");
+ctx.beginPath();
+    ctx.rect(20, 2, 970, 600);
+    ctx.fillStyle = 'indigo';
+    ctx.fill();
+    ctx.lineWidth = 7;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+  // ctx.onload = function {
+  //   context.drawImage(canvas, 0,0);
+  // }
+  // function gameLoop() {
+  //    requestAnimationFrame(gameLoop);
+  //    ctx.fillStyle = "indigo";
+  //    ctx.fillRect(0, 0, 880, 620);
+  // }
 
-class cCircle implements iShape {
-   public x: number = 0;
-   public y: number = 0;
-   public radius: number = 10;
-   public lineWidth: number = 2;
-   public color: string = "red";
-   constructor(x: number, y: number, radius: number, color: string = "red", line_width: number = 2)
-   {
-      this.x = x;
-      this.y = y;
-      this.radius = radius;
-      this.color = color;
-      this.lineWidth = line_width;
-   }
-   public draw = (): void => {
-      ctx.save();
-      ctx.beginPath();
-      ctx.strokeStyle = this.color;
-      ctx.lineWidth = this.lineWidth;
-      ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-      ctx.stroke();
-      ctx.restore();
-   }
-}
-var circle1: cCircle = new cCircle(200, 300, 50);
-var circle2: cCircle = new cCircle(400, 550, 150, "blue", 5);
+let zombie = new Image();
+zombie.onload = function loop() {
 
-function gameLoop() {
-   requestAnimationFrame(gameLoop);
-   ctx.fillStyle = "black";
-   ctx.fillRect(0, 0, 1280, 720);
-   circle1.draw();
-   circle2.draw();
-}
-interface iShape {
-   draw(): void;
-   x: number;
-   y: number;
-   color: string;
-   lineWidth: number;
-}
+  for (let i=400;i < 500; i++){
+    ctx.drawImage(zombie, i,i)
 
-window.onload = () => {
-   canvas = <HTMLCanvasElement>document.getElementById('cnvs');
-   ctx = canvas.getContext("2d");
-   gameLoop();
+function moveZombie() {
+    
+    setInterval(this.zombie, 500);
+
+  }
+
+  }
 }
+  zombie.src ="tiny-geek-zombie-icon.jpg";
+
+
+
+// let canvas: HTMLCanvasElement;
+// let ctx: CanvasRenderingContext2D;
+//
+// class cCircle implements iShape {
+//    public x: number = 0;
+//    public y: number = 0;
+//    public radius: number = 10;
+//    public lineWidth: number = 2;
+//    public color: string = "red";
+//    constructor(x: number, y: number, radius: number, color: string = "red", line_width: number = 2)
+//    {
+//       this.x = x;
+//       this.y = y;
+//       this.radius = radius;
+//       this.color = color;
+//       this.lineWidth = line_width;
+//    }
+//    public draw = (): void => {
+//       ctx.save();
+//       ctx.beginPath();
+//       ctx.strokeStyle = this.color;
+//       ctx.lineWidth = this.lineWidth;
+//       ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+//       ctx.stroke();
+//       ctx.restore();
+//    }
+// }
+// var circle1: cCircle = new cCircle(200, 300, 50);
+// var circle2: cCircle = new cCircle(400, 550, 150, "blue", 5);
+//
+// function gameLoop() {
+//    requestAnimationFrame(gameLoop);
+//    ctx.fillStyle = "black";
+//    ctx.fillRect(0, 0, 880, 620);
+//
+//    if (circle1.x++ >= 880 + circle1.radius) {
+//       circle1.x = -circle1.radius;
+//    }
+//
+//    if (circle2.y++ >= 620 + circle2.radius) {
+//       circle2.y = -circle2.radius;
+//    }
+//
+//    circle1.draw();
+//    circle2.draw();
+// }
+// interface iShape {
+//    draw(): void;
+//    x: number;
+//    y: number;
+//    color: string;
+//    lineWidth: number;
+// }
+//
+// window.onload = () => {
+//    canvas = <HTMLCanvasElement>document.getElementById('cnvs');
+//    ctx = canvas.getContext("2d");
+//    gameLoop();
+// }
