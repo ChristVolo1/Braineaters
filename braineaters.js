4cc0bbaf25274cc8602f298c7adaccac2683f480
ChristVolo1 <christy.volosin@gmail.com>
2016-12-12 22:52:27 -0600

    cleaned up code, working on creating a zombie class.

--- braineaters.js
+++ braineaters.js
@@ -7,14 +7,46 @@ ctx.fill();
 ctx.lineWidth = 7;
 ctx.strokeStyle = 'black';
 ctx.stroke();
+// ctx.onload = function {
+//   context.drawImage(canvas, 0,0);
+// }
+// function gameLoop() {
+//    requestAnimationFrame(gameLoop);
+//    ctx.fillStyle = "indigo";
+//    ctx.fillRect(0, 0, 880, 620);
+// }
+var y = 200;
 var hero = new Image();
 hero.onload = function () {
     ctx.drawImage(hero, 100, 50);
+    // hero.style.position = 'absolute';
+    // hero.style.top = y + 'px';
+    //
+    // document.addEventListener('keydown', function(e) {
+    //   if (e.keyCode==38) {
+    //     y = y - 50;
+    //   }
+    //   if (e.keyCode==40) {
+    //     y = y + 50;
+    //   }
+    //   hero.style.top = y + 'px';
 };
+// );
 hero.src = "tiny-Link_Super_Smash_Bros._Brawl.jpg";
+// }
+// interface zombieCharacter {
+//      draw(): void;
+//      y: number;
+//
+//
+var Zombie = (function () {
+    function Zombie() {
+    }
+    return Zombie;
+}());
 var zombie = new Image();
 zombie.onload = function loop() {
-    var _loop_1 = function (i) {
+    var _loop_1 = function(i) {
         setInterval(function () { ctx.drawImage(zombie, i, 450); }, 5000);
     };
     for (var i = 40; i < 900; i++) {
--- braineaters.ts
+++ braineaters.ts
@@ -15,88 +15,44 @@ ctx.beginPath();
   //    ctx.fillStyle = "indigo";
   //    ctx.fillRect(0, 0, 880, 620);
   // }
+
+
+let y = 200;
 let hero = new Image();
 hero.onload = function() {
   ctx.drawImage(hero, 100, 50)
-}
+  // hero.style.position = 'absolute';
+  // hero.style.top = y + 'px';
+  //
+  // document.addEventListener('keydown', function(e) {
+  //   if (e.keyCode==38) {
+  //     y = y - 50;
+  //   }
+  //   if (e.keyCode==40) {
+  //     y = y + 50;
+  //   }
+  //   hero.style.top = y + 'px';
+  }
+// );
 hero.src="tiny-Link_Super_Smash_Bros._Brawl.jpg"
-// interface zombieCharacter {
-//      draw(): void;
-//      y: number;
-//
-//
-// class Zombie extends zombieCharacter {
+
+
+class Zombie {
+
+  // public speed = Math.random();
+
 let zombie = new Image();
 zombie.onload = function loop() {
-  // if (zombie.y++ >= 900 + zombie) {
-  //     zombie.y = -zombie.;
-  //  }
+  
   for (let i=40;i < 900; i++){
     setInterval(function(){ctx.drawImage(zombie, i,450)},5000);
-
+    
   }
 
   }
   zombie.src ="tiny-geek-zombie-icon.jpg";
 
+// moveLeft = 
 
 
-// let canvas: HTMLCanvasElement;
-// let ctx: CanvasRenderingContext2D;
-//
-// class cCircle implements iShape {
-//    public x: number = 0;
-//    public y: number = 0;
-//    public radius: number = 10;
-//    public lineWidth: number = 2;
-//    public color: string = "red";
-//    constructor(x: number, y: number, radius: number, color: string = "red", line_width: number = 2)
-//    {
-//       this.x = x;
-//       this.y = y;
-//       this.radius = radius;
-//       this.color = color;
-//       this.lineWidth = line_width;
-//    }
-//    public draw = (): void => {
-//       ctx.save();
-//       ctx.beginPath();
-//       ctx.strokeStyle = this.color;
-//       ctx.lineWidth = this.lineWidth;
-//       ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-//       ctx.stroke();
-//       ctx.restore();
-//    }
-// }
-// var circle1: cCircle = new cCircle(200, 300, 50);
-// var circle2: cCircle = new cCircle(400, 550, 150, "blue", 5);
-//
-// function gameLoop() {
-//    requestAnimationFrame(gameLoop);
-//    ctx.fillStyle = "black";
-//    ctx.fillRect(0, 0, 880, 620);
-//
-//    if (circle1.x++ >= 880 + circle1.radius) {
-//       circle1.x = -circle1.radius;
-//    }
-//
-//    if (circle2.y++ >= 620 + circle2.radius) {
-//       circle2.y = -circle2.radius;
-//    }
-//
-//    circle1.draw();
-//    circle2.draw();
-// }
-// interface iShape {
-//    draw(): void;
-//    x: number;
-//    y: number;
-//    color: string;
-//    lineWidth: number;
-// }
-//
-// window.onload = () => {
-//    canvas = <HTMLCanvasElement>document.getElementById('cnvs');
-//    ctx = canvas.getContext("2d");
-//    gameLoop();
-// }
+
